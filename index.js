927909f450d8e096111a6953a1a159fc819207ef
chaeseungyun <thslr57@naver.com>
2024-05-17 00:14:52 +0900

    fix: add mysql2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const admin = require("firebase-admin");
 
 const app = express();
